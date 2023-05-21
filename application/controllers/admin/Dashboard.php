218f3eb48bbedadb4820f005c6c207ddf7ab5278
zlnew <maulana4pz@gmail.com>
2023-05-21 14:45:47 +0700

    add login, register, logout

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
     
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($this->session->userdata('role') != 'admin') {
+            header("HTTP/1.1 401 Unauthorized");
+            exit;
+        }
+    }
+
     public function index()
     {
         $data = [
