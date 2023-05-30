44a61577ac43244c3416a3bb769ea0e713964173
wiwitelgi30 <wiwitelgi30@gmail.com>
2023-05-30 11:20:29 +0700

    profil

---
+++ application/controllers/admin/Profile.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller {
+    
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($this->session->userdata('role') != 'admin') {
+            header("HTTP/1.1 401 Unauthorized");
+            exit;
+        }
+    }
+
+    public function index()
+    {
+        $data = [
+            'content'=>'admin/profile',
+        ];
+    $this->load->view('admin/layouts/app', $data);
+    }
+}
+?>
